f702f2281fecb7fe3f65e8d5554f28277f8a06f5
avrhahm <am500148148@gmail.com>
2026-07-13 08:53:29 +0300

    add routers file

---
+++ routers/order.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const router = express.Router()
+
+router.post("", (req, res) =>{
+})
+
+router.get("", (req,res) =>{
+
+})
+
+router.get("/:id", (req,res) =>{
+    
+})
+router.put("/:id", (req,res) =>{
+    
+})
+
+router.delete("/:id", (req,res) =>{
+    
+})
+
+router.get("/orders/:id/status", (req,res) =>{
+    
+})
