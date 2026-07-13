69127a4fdcffe0157c9b3f3bb204b01e420f475a
avrhahm <am500148148@gmail.com>
2026-07-13 15:16:29 +0300

    update main file

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 import express from 'express'
-import { Logger, chackId } from './utils/middleware'
+import { Logger, checkId, errorHandler, routeNotFound } from './utils/middleware.js'
+import orderRouter from './routers/order.js'
 
 const app = express()
 
 app.use(express.json(), Logger)
-app.use("/orders/:id", chackId )
-app.use("/orders",  )
+app.use('/orders/:id', checkId)
+app.use('/orders', orderRouter)
+
+app.use(routeNotFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+export default app
