0a9e6a9dabb9163f9faf0820dd390474e0a8927e
avrhahm <am500148148@gmail.com>
2026-07-13 15:15:52 +0300

    updatae routers

--- routers/order.js
+++ routers/order.js
@@ -1,25 +1,61 @@
 import express from 'express'
+import * as service from '../service/order.js'
+import { validateOrder } from '../utils/middleware.js'
 
 const router = express.Router()
 
-router.post("", (req, res) =>{
+router.post('/', validateOrder, async (req, res, next) => {
+    try {
+        const result = await service.createOrder(req.body)
+        res.status(result.status).json(result.data)
+    } catch (e) {
+        next(e)
+    }
 })
 
-router.get("", (req,res) =>{
-
+router.get('/', async (req, res, next) => {
+    try {
+        const result = await service.getOrder(req.query)
+        res.status(result.status).json(result.data)
+    } catch (e) {
+        next(e)
+    }
 })
 
-router.get("/:id", (req,res) =>{
-    
-})
-router.put("/:id", (req,res) =>{
-    
-})
-
-router.delete("/:id", (req,res) =>{
-    
+router.get('/:id', async (req, res, next) => {
+    try {
+        const result = await service.getOrderByID(Number(req.params.id))
+        res.status(result.status).json(result.data)
+    } catch (e) {
+        next(e)
+    }
 })
 
-router.get("/orders/:id/status", (req,res) =>{
-    
+router.put('/:id', validateOrder, async (req, res, next) => {
+    try {
+        const result = await service.updateOrder(Number(req.params.id), req.body)
+        res.status(result.status).json(result.data)
+    } catch (e) {
+        next(e)
+    }
 })
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const result = await service.deleteOrder(Number(req.params.id))
+        res.status(result.status).json(result.data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.patch('/:id/status', async (req, res, next) => {
+    try {
+        const result = await service.changeStatus(Number(req.params.id), req.body)
+        res.status(result.status).json(result.data)
+    } catch (e) {
+        next(e)
+    }
+})
+
+export default router
